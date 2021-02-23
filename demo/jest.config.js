66ce4f9e531181a73f0feeba47d01b9dc0636787
Dong <nguyentandong0511@gmail.com>
2021-02-23 11:30:53 +0700

    add app3

--- demo/jest.config.js
+++ demo/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/app-test', '<rootDir>/apps/app-test2'],
+  projects: [
+    '<rootDir>/apps/app-test',
+    '<rootDir>/apps/app-test2',
+    '<rootDir>/apps/app-test3',
+  ],
 };
